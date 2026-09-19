8a14ef708f2e3a2f450cf12dbb91bd98b3a28022
LatteOtta <nathlter@outlook.com>
2026-09-19 13:34:52 +0200

    lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,84 +3,30 @@ return {
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			local function vimicon()
-				return [[]]
-			end
 			require("lualine").setup({
 				options = {
 					component_separators = { left = "", right = "" },
 					section_separators = { left = "", right = "" },
 					-- component_separators = { left = "", right = "" },
 					-- section_separators = { left = "", right = "" },
-					disabled_filetypes = {
-						-- "tex",
-					},
 				},
 				sections = {
-					lualine_a = {
-						{ vimicon, separator = { left = "", right = "" } },
-						{ "mode", separator = { left = "", right = "" } },
+					lualine_c = {
+						{
+							"filename",
+							path = 3,
+						},
 					},
+					lualine_x = {},
 				},
 			})
+			if vim.bo.filetype == "tex" then
+				require("lualine").setup({
+					sections = {
+						lualine_b = {},
+					},
+				})
+			end
 		end,
 	},
 }
-
---[[
--- default config 
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    always_show_tabline = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-      refresh_time = 16, -- ~60fps
-      events = {
-        'winenter',
-        'bufenter',
-        'bufwritepost',
-        'sessionloadpost',
-        'filechangedshellpost',
-        'vimresized',
-        'filetype',
-        'cursormoved',
-        'cursormovedi',
-        'modechanged',
-      },
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
---]]
